1866fde72c4b646b1518948676d9e2663dd0bd8a
purskyi <purskyipro@gmail.com>
2024-09-17 13:31:57 +0300

    finished

--- src/main/java/com/example/googleheros/MainActivity.kt
+++ src/main/java/com/example/googleheros/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -23,9 +24,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,6 +41,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -60,11 +65,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SuperHeroesApp() {
-    LazyColumn() {
-        items(HeroesRepository.heroes) { hero ->
-            HeroCard(hero)
+    Scaffold(topBar = { TopAppBarWithAppName() })
+    { it ->
+        LazyColumn(contentPadding = it) {
+            items(HeroesRepository.heroes) { hero ->
+                HeroCard(hero)
+            }
         }
     }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarWithAppName(modifier: Modifier = Modifier) {
+   /* CenterAlignedTopAppBar(
+        title = { Image(modifier = modifier
+            .size(72.dp)
+            .padding(8.dp),
+        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+        contentDescription = null ) }
+    )*/
+    CenterAlignedTopAppBar(
+        title = {
+            Text(text = stringResource(id = R.string.app_name),
+                fontWeight = FontWeight.Bold)
+        })
+
+
 }
 
 
@@ -84,8 +110,7 @@ Box(modifier = modifier){
                 .height(104.dp)
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.Absolute.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
+            verticalAlignment = Alignment.Top) {
 
             Information(hero = hero,modifier.weight(1f))
          //   Spacer(modifier = Modifier.weight(1f))
@@ -102,6 +127,7 @@ fun InfoImage(
     Image(painter = painterResource(id = hero.imageRes),
         contentDescription = null ,
         modifier = modifier
+            .padding(16.dp)
             .size(72.dp)
             .aspectRatio(1f)
             .clip(RoundedCornerShape(8.dp)),
@@ -114,11 +140,12 @@ fun Information(
     hero: Hero,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier
+        .padding(16.dp)) {
         Text(
             text = stringResource(hero.name),
             style = MaterialTheme.typography.labelLarge,
-            modifier = Modifier.padding(top = 8.dp)
+            fontWeight = FontWeight.Bold
         )
         Text(
             text = stringResource(hero.description),
